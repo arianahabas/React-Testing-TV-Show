c3d6486ed6006a36b9d3b634100de5076b837169
arianahabas <arianahabas@gmail.com>
2020-08-18 11:02:35 -0500

    episodes tests done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
+import { fetchShow } from './api/fetchShow'
 
 import { formatSeasons } from "./utils/formatSeasons";
 
@@ -15,17 +16,14 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
+    const getShow = () => {
+    fetchShow()
         .then(res => {
           setShow(res.data);
           setSeasons(formatSeasons(res.data._embedded.episodes));
         });
     };
-    fetchShow();
+    getShow();
   }, []);
 
   const handleSelect = e => {
@@ -35,7 +33,7 @@ export default function App() {
   if (!show) {
     return <h2>Fetching data...</h2>;
   }
-
+  console.log(episodes)
   return (
     <div className="App">
       <img className="poster-img" src={show.image.original} alt={show.name} />
